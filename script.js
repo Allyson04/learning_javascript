95ab4d7369fd2d0dd676885a6ceaded4a96b6c0b
Allyson <allysoneduardo04@gmail.com>
2020-12-28 17:26:29 -0300

    feat: adicionada faixa etária completa

---
+++ script.js
@@ -0,0 +1,11 @@
+var idade = prompt("Insira aqui sua idade:").toString();
+
+if (0 <= idade && idade < 15) {
+    document.write("Criança");
+} else if (15 <= idade && idade < 30) {
+    document.write("Jovem");
+} else if (30 <= idade && idade < 60) {
+    document.write("Adulto");
+} else if (60 <= idade) {
+    document.write("Idoso");
+}
