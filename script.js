ff76509cfaf8d00919e09377b9238fe81c1537d2
Allyson <allysoneduardo04@gmail.com>
2020-12-28 14:17:29 -0300

    feat: added scenario 'if' student don't have enough frequency

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // or => || => true if any expression is true
 // not => ! => true invert the result of comparison
 
-var subject1 = 5;
+var subject1 = prompt("Grade for subject1:");
 var subject2 = 8;
 var subject3 = 10;
 var attendance = 10;
@@ -11,4 +11,6 @@ var absence = prompt("Number of days when you was late or absent:")
 
 if (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence >= 8) {
     document.write("Congratulations! You passed the exams and got the necessary frequency!")
+} else if (attendance - absence < 8) {
+    document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him, at least your grades were good enough!")
 } 
