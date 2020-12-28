b5e00a910366cfa24216b33201beb0760a9424f6
Allyson <allysoneduardo04@gmail.com>
2020-12-28 14:26:55 -0300

    feat: checking first if data is valid

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ var subject2 = 8;
 var subject3 = 10;
 var attendance = 10;
 var averageScore = 7;
-var absence = prompt("Number of days when you was late or absent:")
+var absence = prompt("Number of days when you was late or absent:");
 
-if (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence >= 8) {
+if (attendance + 1 <= attendance - absence || attendance - absence <= -1 || subject1 >= 11 || subject2 >= 11 || subject3 >= 11) {
+    document.write("There was a problem with data inserted, please check if the info were correct and try again")
+} else if (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence >= 8) {
     document.write("Congratulations! You passed the exams and got the necessary frequency!")
 } else if (attendance - absence < 8) {
-    document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him, at least your grades were good enough!")
+    document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him.")
 } else if (subject1/3 + subject2/3 + subject3/3 < averageScore) {
     document.write("Sorry, but your grades were not enough for your approval, study more so your grades became better. Remember to always take a break and drink water!")
-} 
+}
