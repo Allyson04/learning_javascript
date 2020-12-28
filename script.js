0ffdabd7dd9474c6a8826ef0abe0f82017a1447c
Allyson <allysoneduardo04@gmail.com>
2020-12-28 14:18:16 -0300

    feat: added scenario if student's grades aren't enough

--- script.js
+++ script.js
@@ -13,4 +13,6 @@ if (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence
     document.write("Congratulations! You passed the exams and got the necessary frequency!")
 } else if (attendance - absence < 8) {
     document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him, at least your grades were good enough!")
+} else if (subject1/3 + subject2/3 + subject3/3 < averageScore) {
+    document.write("Sorry, but your grades were not enough for your approval, study more so your grades became better. Remember to always take a break and drink water!")
 } 
