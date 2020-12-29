b6b504427c20d12f84cb4d44bd7534a462e3261d
Allyson <allysoneduardo04@gmail.com>
2020-12-29 19:02:34 -0300

    feat: added BMI variable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var name = prompt("What is your name?");
 var height = parseFloat(prompt("How tall are you? (in centimeters)"));
 var weight = parseFloat(prompt("How much do you weigh? (in kilograms)"));
+var BMI = null;
 
 height /= 100
 
