77de6ce1ccf248b3689de233091dc8e3bc52ec10
Allyson <allysoneduardo04@gmail.com>
2020-12-29 18:40:21 -0300

    refactor: instead of dividing score's subject by 3, dividing total score for 3

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ var attendance = 10;
 var averageScore = 7;
 var absence = prompt("Number of days when you was late or absent:");
 
-// if (attendance + 1 <= attendance - absence || attendance - absence <= -1 || subject1 >= 11 || subject2 >= 11 || subject3 >= 11) {
-//     document.write("There was a problem with data inserted, please check if the info were correct and try again")
-// } else if (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence >= 8) {
-//     document.write("Congratulations! You passed the exams and got the necessary frequency!")
-// } else if (attendance - absence < 8) {
-//     document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him.")
-// } else if (subject1/3 + subject2/3 + subject3/3 < averageScore) {
-//     document.write("Sorry, but your grades were not enough for your approval, study more so your grades became better. Remember to always take a break and drink water!")
-// } else {
-//     document.write("hey, what are you doing here?? get away from here")
-// }
+if (attendance + 1 <= attendance - absence || attendance - absence <= -1 || subject1 >= 11 || subject2 >= 11 || subject3 >= 11) {
+    document.write("There was a problem with data inserted, please check if the info were correct and try again")
+} else if ((subject1 + subject2 + subject3)/3 >= averageScore && attendance - absence >= 8) {
+    document.write("Congratulations! You passed the exams and got the necessary frequency!")
+} else if (attendance - absence < 8) {
+    document.write("Sorry, but you were absent in too much classes! Find your teacher to discuss this problem with him.")
+} else if (subject1/3 + subject2/3 + subject3/3 < averageScore) {
+    document.write("Sorry, but your grades were not enough for your approval, study more so your grades became better. Remember to always take a break and drink water!")
+} else {
+    document.write("hey, what are you doing here?? get away from here")
+}
 
-var result = (subject1/3 + subject2/3 + subject3/3 >= averageScore && attendance - absence >= 8) ? 'Aprovado' : 'Reprovado'
+// var result = ((subject1 + subject2 + subject3)/3 >= averageScore && attendance - absence >= 8) ? 'Aprovado' : 'Reprovado'
 
-document.write(result)
+// document.write(result)
