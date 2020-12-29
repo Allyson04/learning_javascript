0fab45720c98e692ebb584efb184f5668c02b912
Allyson <allysoneduardo04@gmail.com>
2020-12-29 19:01:06 -0300

    feat: converting centimeters to meters

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var name = prompt("What is your name?");
-var height = parseFloat(prompt("How tall are you? (in centimeters"));
+var height = parseFloat(prompt("How tall are you? (in centimeters)"));
 var weight = parseFloat(prompt("How much do you weigh? (in kilograms)"));
+
+height /= 100
 
