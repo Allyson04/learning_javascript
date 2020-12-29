63dd92e055906ccb39e88b7ea3477b521ff4327e
Allyson <allysoneduardo04@gmail.com>
2020-12-29 14:13:51 -0300

    learning arithmetical operators

---
+++ script.js
@@ -0,0 +1,19 @@
+// Arithmetical Operators
+
+// + => Only works with Number, if its values are different, then it concanate
+// - => Difference between two Numbers
+// * => Product
+// "/" => Division
+// % => "Module" uses the rest in a Division
+// ++ => Incremment of 1 (if the symbol comes before the number, it display instantly the new value, but if used after, it refresh the value only after all the expressions)
+// -- => Decrement of 1 (see incremment)
+
+var num1 = 5;
+var num2 = 3;
+
+document.write(
+    "Testing some expressions with " + num1 + " and " + num2 + "<br />" +
+    num1 + num2
+
+    In this example we are concanating strings and numbers, instead of 
+)
