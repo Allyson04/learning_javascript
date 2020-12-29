26eb530f1f2b48539486cf22199596c0bc775d1a
Allyson <allysoneduardo04@gmail.com>
2020-12-29 18:52:29 -0300

    feat: converting strings to fraction number

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var name = prompt("What is your name?");
-var height = prompt("How tall are you?");
-var weight = prompt("How much do you weigh?");
+var name = parseFloat(prompt("What is your name?"));
+var height = parseFloat(prompt("How tall are you?"));
+var weight = parseFloat(prompt("How much do you weigh?"));
+
+
