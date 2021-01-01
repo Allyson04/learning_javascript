c2c51155e49e89bc0190c8273d34f0002818839a
Allyson <allysoneduardo04@gmail.com>
2021-01-01 11:01:03 -0300

    feat: learning basic concepts about functions' flexibility, with more or less values

---
+++ script.js
@@ -0,0 +1,8 @@
+function sum (a, b) {
+    return a + b
+}
+
+console.log(sum(7, 7, 9, 15))
+ //when there is more than values than it was declared in function, it use the first ones declared  an discards the rest
+
+//if it has less values than it was declared, than the ones missing becomes undefined
