94ddcb24b04999a3c906624dcb4c93a4ba90d264
Allyson <allysoneduardo04@gmail.com>
2021-01-02 00:16:25 -0300

    fat: learning how to manipulate

---
+++ script.js
@@ -0,0 +1,26 @@
+var name = ' Jorge Sant Ana '
+
+//what is the length of the string
+// document.write(name.length)
+
+//Finding an specific letter in the strings based on position
+// document.write(name.charAt(0))
+
+//Finding the first encounter of specific value in a string
+// document.write(name.indexOf("a"))
+
+//replacing an specific value within a string
+// document.write(name.replace("a", "i"))
+// document.write(name.replace("Sant Ana", "Silva Ribeiro"))
+
+
+// document.write(name.substr(6, 4))
+
+//transform all string in lower case letters
+// document.write(name.toLowerCase())
+
+//transform all string in upper case letters
+// document.write(name.toUpperCase())
+
+//removes blank spaces in string
+// document.write('-' + name.trim() + '-')
