76c73f6558525c799bb30fb3593aff557e1aa44c
Allyson <allysoneduardo04@gmail.com>
2021-01-03 18:25:55 -0300

    feat: added anonymous function with to call length, temperature and weight converters

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 alert("Welcome to our site! We are here to help you with all we can do")
 
+var operation = prompt("First of all, what do you need? A length converter, a weight converter or a temperature converter")
+
+var searchOperation = function(operation) {
+    var numHave = prompt("what is the value you have?")
+    var unitHave = prompt("What is the unit you have?")
+    var unitWant = prompt("What is the unit you want?")
+    
+    if (operation == "length converter") {
+        convertLengths(numHave, unitHave, unitWant)
+    }
+
+    if (operation == "weight converter") {
+        convertWeight(numHave, unitHave, unitWant)
+    }
+
+    if (operation == "temperature converter") {
+        convertTemperature(numHave, unitHave, unitWant)
+    }
+}
