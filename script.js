efb708b8e69890881ec60271353bbc08c8504f29
Allyson <allysoneduardo04@gmail.com>
2021-01-06 19:58:11 -0300

    feat: training mouse events

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Mouse events
 
-onclick //used for clicks
-ondblclick //used for double clicks
-onmouseup //used for when pressing a button
-onmouseover //used for hovering an element
-onmouseout //used for getting out of an element
+// onclick //used for clicks
+// ondblclick //used for double clicks
+// onmouseup //used for when pressing a button, if it stays then nothing happen, only after holding the button
+// onmouseover //used for hovering an element
+// onmouseout //used for getting out of an element
+
+function actionClick () {
+    alert("Element was clicked")
+}
+
+function actionOut () {
+    alert("Mouse Out!")
+}
+
+function actionDblClick () {
+    alert("One Click is enough! Two is too much!!")
+}
+
+function actionOver () {
+    alert("Mouse is hovering this element...")
+}
+
