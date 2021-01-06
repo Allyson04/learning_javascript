31a86d243055048811d89ad7cf64ef001cc43480
Allyson <allysoneduardo04@gmail.com>
2021-01-06 20:17:49 -0300

    feat: added divs with description of their attributes

--- script.js
+++ script.js
@@ -6,19 +6,19 @@
 // onmouseover //used for hovering an element
 // onmouseout //used for getting out of an element
 
-function actionClick () {
+function MouseClick () {
     alert("Element was clicked")
 }
 
-function actionOut () {
+function MouseOut () {
     alert("Mouse Out!")
 }
 
-function actionDblClick () {
+function MouseDblClick () {
     alert("One Click is enough! Two is too much!!")
 }
 
-function actionOver () {
+function MouseHover () {
     alert("Mouse is hovering this element...")
 }
 
