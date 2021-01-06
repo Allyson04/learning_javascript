d853961703f291255ce53389ecba9459262835b9
Allyson <allysoneduardo04@gmail.com>
2021-01-06 19:34:50 -0300

    feat: learning mouse events

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mouse events
+
+onclick //used for clicks
+ondblclick //used for double clicks
+onmouseup //used for when pressing a button
+onmouseover //used for hovering an element
+onmouseout //used for getting out of an element
