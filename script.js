abecd28e4f350ae544fd8f48a8347ecb03cd116c
Allyson <allysoneduardo04@gmail.com>
2021-01-08 16:19:58 -0300

    feat: adding keyboard functions for cards in index.html

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ function MouseHover () {
 onkeydown //used when a key is lowered by pressing
 onkeyup // when a key is released 
 onkeypress // this one is tricky, it's represent the moment after the key in lowered, but before it release, so a key stays pressed. It only used for keys with a character, so alphabets, numerics and punctuation marks are valid. Ctrl, Del, Caps Lock and similar don't use this event
+
+function keyDown () {
+    alert("message activated")
+}
+
+function keyPress () {
+    alert("")
+}
+
+function keyUp () {
+    alert("")
+}
