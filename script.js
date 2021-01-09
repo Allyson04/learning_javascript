76b3aa71c5bddad302b5ead5c07d1a7f6eeed899
Allyson <allysoneduardo04@gmail.com>
2021-01-09 17:10:46 -0300

    feat: selecting input#user_email

--- script.js
+++ script.js
@@ -4,3 +4,5 @@
 // getElementsByTagName() //Catches Elements by Tag Name, for example, links is uses <a>, images uses <img>, and go on
 // getElementsByClassName() //Catches Elements by class name, like CSS
 // getElementsByName() // Catches Elements by Name, like Name="radio_button"
+
+// Considering that script is read before HTML, then we are using an script tag to use Javascript, the code is in index.html
