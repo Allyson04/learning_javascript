71a07475ead0c51e10d7f60b5501192fa6ceeec1
Allyson <allysoneduardo04@gmail.com>
2021-01-09 16:56:07 -0300

    feat: learning how to select elements by ID, TagName, ClassName and Name

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM Manipulation
+
+// getElementById() //Catches Element by his Id, remember that Id is intended to be used once
+// getElementsByTagName() //Catches Elements by Tag Name, for example, links is uses <a>, images uses <img>, and go on
+// getElementsByClassName() //Catches Elements by class name, like CSS
+// getElementsByName() // Catches Elements by Name, like Name="radio_button"
