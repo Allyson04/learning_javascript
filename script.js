9038f4c7f10d42118a8d33b2344613eca5557d8c
Allyson <allysoneduardo04@gmail.com>
2021-01-09 18:06:11 -0300

    feat: created an application to separate numbers and words

--- script.js
+++ script.js
@@ -6,3 +6,42 @@
 // getElementsByName() // Catches Elements by Name, like Name="radio_button"
 
 // Considering that script is read before HTML, then we are using an script tag to use Javascript, the code is in index.html
+
+function distributionOfCharacters() {
+
+    //here we are selecting an element by Id and then extracting the value within
+    var characterInputed = document.getElementById('input_characters').value
+
+    //Now we erase the value in the element
+    document.getElementById('input_characters').value = ""
+
+    // Erasing blank spaces between strings
+    characterInputed.trim()
+
+    // because the value inserted within inputs are always identified as strings, we decide to use switch, below this we tried using if-else but couldn't think of a way to do 
+    switch (characterInputed) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        document.getElementById("input_numbers").value += characterInputed
+
+        break
+
+        default:
+        document.getElementById("input_words").value += characterInputed
+
+        break
+    }
+
+    // Can we do it with if-else instead of switch?
+    // if (characterInputed == Number) {
+    //     document.getElementById("input_number").value = document.getElementById("input_number").value + characterInputed
+    // }
+}
