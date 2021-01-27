c5acfbf372e42b2a4215e8c8a6adaf856222b68b
Allyson <allysoneduardo04@gmail.com>
2021-01-27 14:23:18 -0300

    feat: added explanation about multidimensional Arrays

---
+++ multidimensional_arrays/script.js
@@ -0,0 +1,25 @@
+//if we have an important array
+let elements = [
+    "potassium",
+    "carbon",
+    "iron",
+    "oxygen",
+    "osmium",
+    "copper",
+    "zinc"
+]
+
+//and other array
+let energy = [
+    "light",
+    "sound"
+]
+
+//can we combine them in other array?
+//Yes! We can do this! This is called multidimensional Array
+let existent_things = [
+    energy,
+    elements
+]
+
+console.log(existent_things)
