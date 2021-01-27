ded38c8803a33fdd3ea13ef190ba41895ef6ea68
Allyson <allysoneduardo04@gmail.com>
2021-01-26 22:16:06 -0300

    feat: learned how to add values to arrays

--- basics/script.js
+++ basics/script.js
@@ -6,20 +6,6 @@ let schools_list = []
 let houses_list = array('cabin', "'home", 'wood')
 let backpack_list = ['plastic', "polyester", 'cloth']
 
-
 //adding values to this new array
 names_list[1] = "Allyson"
 names_list[2] = "Eduardo"
-
-//we can add not only strings, but numbers
-names_list[3] = 4
-names_list[10] = 7
-
-//even binarys
-names_list[5] = true
-names_list[6] = false
-
-//also, we don't need to use only numbers for searching index
-
-names_list["p"] = "paulo"
-names_list["text"] = "Lorem Ipsum"
