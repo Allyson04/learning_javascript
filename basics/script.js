348bf318269831e5df4db016357390e7bebbc58b
Allyson <allysoneduardo04@gmail.com>
2021-01-26 22:08:44 -0300

    feat: added possibility in creating arrays

---
+++ basics/script.js
@@ -0,0 +1,7 @@
+//Creating an arrays
+let names_list = array()
+let schools_list = []
+
+//we can also create non-defined arrays
+let houses_list = array('cabin', "'home", 'wood')
+let backpack_list = ['plastic', "polyester", 'cloth']
