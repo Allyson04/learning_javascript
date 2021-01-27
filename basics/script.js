c4274bf101193ebfe5bf10c59b5498fff9bc85ad
Allyson <allysoneduardo04@gmail.com>
2021-01-27 13:58:59 -0300

    feat: added difference between length in arrays and in strings

--- basics/script.js
+++ basics/script.js
@@ -7,18 +7,22 @@ let houses_list = Array('cabin', "'home", 'wood')
 let backpack_list = ['plastic', "polyester", 'cloth']
 
 //adding values to this new array
-names_list[1] = "Allyson"
-names_list[2] = "Eduardo"
+names_list[0] = "Allyson"
+names_list[1] = "Eduardo"
 
 //we can add not only strings, but numbers
-names_list[3] = 4
-names_list[10] = 7
+names_list[2] = 4
+names_list[3] = 7
 
 //even binarys
-names_list[5] = true
-names_list[6] = false
+names_list[4] = true
+names_list[5] = false
 
 //also, we don't need to use only numbers for searching index
 
 names_list["p"] = "paulo"
 names_list["text"] = "Lorem Ipsum"
+
+
+//when length is used for arrays it displays the number of values in this array, but array's positions starts with 0, and when used length it display the absolute value, always +1 than the last position
+console.log(names_list.length)
