97886a8ab75ca2d1d5e1f163898fddf26a8228c9
Allyson <allysoneduardo04@gmail.com>
2021-01-28 19:21:00 -0300

    feat: added how to exclude and element in the end or start of an array

--- array_include_exclude/script.js
+++ array_include_exclude/script.js
@@ -6,7 +6,12 @@ fruits[1] = "pear"
 //adding an element to the end of an array
 fruits.push("watermelon")
 
-//adding an elements to the beginning of and array
+//adding an elements to the beginning of an array
 fruits.unshift("grape")
 
+// excluding an element in the end of an array
+fruits.pop()
+
+//excluding an elements at the start of an array
+fruits.shift()
 console.log(fruits)
