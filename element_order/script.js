91d7245d68c477fc6dbb06d79b65a06c39fcdfd1
Allyson <allysoneduardo04@gmail.com>
2021-01-31 13:03:31 -0300

    feat: added 'addElement' function to add elements in array 'objects'

--- element_order/script.js
+++ element_order/script.js
@@ -1,6 +1,22 @@
 let objects = ["chair", "table", "fork"]
 
-function addElement(objects) {
+function addElement() {
     let inputValue = document.getElementById("elementInput").value
+    console.log(inputValue)
     
+    if (inputValue === "") {
+        alert("Please, insert something.")
+    } else {
+        let elementSearch = objects.indexOf(inputValue)
+        console.log(elementSearch)
+
+        if (elementSearch == "-1") {
+            objects.push(inputValue)
+            console.log(objects)
+
+            document.getElementById("elementInput").value = ""
+        } else if (elementSearch !== "-1") {
+            alert("This Element was already added, try a new one.")
+        }
+    }
 }
