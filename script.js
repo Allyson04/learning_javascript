27c6188440b9fcae63706be200ac72844174fd93
Allyson <allysoneduardo04@gmail.com>
2021-03-20 14:28:58 -0300

    feat: added basic while structure

---
+++ script.js
@@ -0,0 +1,27 @@
+// //defining value of variable x
+// x = 1
+
+// while(x <= 10) {
+//     //do the function
+//     document.write(x + '<br />')
+
+//     //and add 1 for x
+//     x += 2
+// }
+
+x = 1
+
+// console.log("Start!")
+
+// while(x <= 10) {
+//     console.log(x)
+
+//     //here if x equals 5, then the function breaks and goes to the next step, in this case, the console.log
+//     if(x == 5){
+//         break
+//     }
+
+//     x++
+// }
+
+// console.log("The loop ended")
