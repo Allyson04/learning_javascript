14a319a5c798f1844512413dbe06e89c92164cb6
Allyson <allysoneduardo04@gmail.com>
2021-03-20 14:31:14 -0300

    feat: added basic while structure with break

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-//defining value of variable x
+// //defining value of variable x
+// x = 1
+
+// while(x <= 10) {
+//     //do the function
+//     document.write(x + '<br />')
+
+//     //and add 1 for x
+//     x += 2
+// }
+
 x = 1
 
-while(x <= 10) {
-    //do the function
-    document.write(x + '<br />')
+console.log("Start!")
 
-    //and add 1 for x
-    x += 2
+while(x <= 10) {
+    console.log(x)
+
+    if(x == 5){
+        break
+    }
+
+    x++
 }
+
+console.log("The loop ended")
