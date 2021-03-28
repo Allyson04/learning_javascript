c4948771a337f488e72ea9eda8ab02d65a444e21
Allyson <allysoneduardo04@gmail.com>
2021-03-27 23:08:50 -0300

    feat: added 'for in' loop

--- script.js
+++ script.js
@@ -97,3 +97,32 @@ y = 0
 //     }
 //     document.write("<hr />")
 // }
+
+inviteList = ['Jorge', 'Jamilton', 'José', 'Ana', 'Maria']
+
+console.log(inviteList)
+
+//in is what separates the variable "x" from the variable we are using to search
+for(x in inviteList) {
+    document.write(inviteList[x] + "<br />")
+
+    //as we can see, we dont need to define the limit of the looping or how much it add (x++)
+}
+
+document.write("<hr />")
+
+// other advantage is that if we use other indexes in arrays, as we see on the other example, the for in loop uses a index it defines alone
+
+inviteList = Array()
+
+inviteList[2] = "Jorge"
+inviteList[-1] = "Jamilton"
+inviteList[true] = "José"
+inviteList["zebra"] = "Ana"
+inviteList[2.1] = "Maria"
+
+for(x in inviteList) {
+    document.write("index: " + x + "<br />" + "value: " + inviteList[x] + "<br />" + "<br />")
+
+    //as we can see, we dont need to define the limit of the looping or how much it add (x++)
+}
